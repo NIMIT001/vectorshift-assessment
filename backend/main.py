106ff0c156290fe3950ef0861ffe7e3d2c16fb42
NIMIT001 <nimitsoni2002@gamil.com>
2025-12-28 19:54:28 +0530

    final

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Local development
-        "https://vectorshift-frontend-chi.vercel.app",  # Production frontend
+        "https://vectorshift-assessment-delta.vercel.app/",  # Production frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
